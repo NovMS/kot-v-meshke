6909490932031ca2288a22090021e30232fbd206
Maksim Novoseltsev <no.ms@ya.ru>
2018-03-23 02:40:59 +0300

    Первый коммит

---
+++ Gruntfile.js
@@ -0,0 +1,143 @@
+module.exports = function(grunt) {
+	require("load-grunt-tasks")(grunt);
+		
+	grunt.initConfig({
+		
+		less: {
+			style: {
+				files: {
+					"build/css/style.css": "less/style.less"
+				}
+			}
+		},
+		
+		postcss: {
+			style: {
+				options: {
+					processors: [
+						require("autoprefixer")({browsers: [
+							"last 1 version",
+							"last 2 Chrome versions",
+							"last 2 Firefox versions",
+							"last 2 Opera versions",
+							"last 2 Edge versions"
+						]}),
+						require("css-mqpacker")({
+							sort: true
+						})
+					]
+				},
+				src: "build/css/*.css"
+			}
+		},
+		
+		watch: {
+			html: {
+				files: ["*.html"],
+				tasks: ["copy:html"]
+			},
+			style: {
+				files: ["less/**/*.less"],
+				tasks: ["less", "postcss", "csso"]
+			}
+		},
+		
+		browserSync: {
+			server: {
+				bsFiles: {
+					src: ["build/*.html", "build/css/*.css"]
+				},
+				options: {
+					server: "build",
+					watchTask: true
+				}
+			}
+		},
+		
+		csso: {
+			style: {
+				options: {
+					report: "gzip"
+				},
+				files: {
+					"build/css/style.min.css": ["build/css/style.css"]
+				}
+			}
+		},
+		
+		imagemin: {
+			images: {
+				options: {
+					optimizationLevel: 3
+				},
+				files: [{
+					expand: true,
+					src: ["build/img/**/*.{png,jpg,gif}"]
+				}]
+			}
+		},
+		
+		svgstore: {
+			options: {
+				svg: {
+					style: "display: none",
+					xmlns: "http://www.w3.org/2000/svg"
+				}
+			},
+			symbols: {
+				files: {
+					"build/img/symbols.svg": ["img/icons/*.svg"]
+				}
+			}
+		},
+		
+		svgmin: {
+			symbols: {
+				files: [{
+					expand: true,
+					src: ["build/img/icons/*.svg"]
+				}]
+			}
+		},
+		
+		copy: {
+			build: {
+				files: [{
+					expand: true,
+					src: [
+						"fonts/**/*.{woff,woff2}",
+						"img/**",
+						"js/**",
+						"*.html",
+						"css/normalize.min.css"
+					],
+					dest: "build"
+				}]
+			},
+			html: {
+				files: [{
+					expand: true,
+					src: ["*.html"],
+					dest: "build"
+				}]
+			}
+		},
+		
+		clean: {
+			build: ["build"]
+		}
+	});
+	
+	grunt.registerTask("serve", ["browserSync", "watch"]);
+	grunt.registerTask("symbols", ["svgmin", "svgstore"]);
+	grunt.registerTask("build", [
+		"clean",
+		"copy",
+		"less",
+		"postcss",
+		"csso",
+		"symbols",
+		"imagemin"
+	]);
+	
+};
